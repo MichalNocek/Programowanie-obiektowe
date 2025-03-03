970fbc6a6ab62f63559f7a822eca1ab6daeb573f
MichalNocek <michaln057@gmail.com>
2025-03-03 10:38:38 +0100

    folder zajecia

--- Zajecia/src/Main.java
+++ Zajecia/src/Main.java
@@ -1,6 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+
+      //  int[] arr;//deklaracja tablicy
+      //  // arr = new int[5];//inicjowanie
+//      int [] arr = new int[5];
+//      int[] arr1 = {12,32,43,2,1,-23,12};
+//
+//      int [] arr = {1,2,3,4,32,123};
+//
+//      for (int i =0;i<arr.length;i++){
+//          System.out.println("i= "+arr[i]);
+//      }
+//
+//      //foreach
+//
+//      for(int item : arr){
+//          System.out.println(item);
+//      }
+//
+
+        int[][] macierz ={
+                {1,2,3,4},
+                {1,2,3,4,5,6}
+        };
+
+//
+//        for(int i=0;i<macierz.length;i++){
+//            for (int j =0;j<macierz[i].length;j++)
+//                System.out.println(macierz[i][j]);
+//        }
+
+//        for(int[] item: macierz){
+//            for(int data:item){
+//                System.out.println(data+ " ");
+//            }
+//        }
+//
+//
+
+
+
+  }
+
 
 }
