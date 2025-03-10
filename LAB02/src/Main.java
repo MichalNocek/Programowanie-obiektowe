a9c554a05e898bb07bed5b5fe120500953817e94
MichalNocek <michaln057@gmail.com>
2025-03-10 14:10:13 +0100

    Lab 02 uzupełniony

---
+++ LAB02/src/Main.java
@@ -0,0 +1,16 @@
+import Input_random.Input;
+import Input_random.Random_number;
+import Input_random.Run;
+
+import java.util.Random;
+
+public class Main {
+    static Run run = new Run();
+
+
+    public static void main(String[] args) {
+
+    run.RunTask();
+
+    }
+}
